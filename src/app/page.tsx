913586f2e45a1bef74fd99b6e4fba1b1f8f58607
Septian A Tama <sfirst.tama@gmail.com>
2023-03-01 16:03:07 +0700

    get list of puzzles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,35 @@
-import _times from 'lodash/times';
+import Link from 'next/link';
+import { supabase } from '~/lib/supabaseClient';
 
-const Home = () => {
-  // static data sample
-  const sudokuStr =
-    '634.28.15...456283528.13.6.45.397128213865.4.8..14.5.6.6.58..91381279654945631872';
+async function getPuzzles() {
+  const { data, error } = await supabase.from('sudoku_puzzles').select();
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-  // generate sudoku rows
-  const sudokuRows = _times(9, (round: number) => {
-    const rowStart = round * 9;
-    const rowEnd = (round + 1) * 9;
-    return sudokuStr.slice(rowStart, rowEnd).split('');
-  });
+  // Recommendation: handle errors
+  if (error) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
 
+  return data;
+}
+
+const Home = async () => {
+  const puzzles = await getPuzzles();
   return (
     <div className="container mx-auto my-8 px-8">
-      <h1 className="text-3xl font-bold underline mb-4">{`Let's play SUDOKU!`}</h1>
-      <div className="grid grid-rows-9 gap-0">
-        {sudokuRows.map((row, rowIdx) => (
-          <div className="grid grid-cols-9 gap-0" key={`row-${rowIdx}`}>
-            {row.map((col, colIdx) => (
-              <div
-                className="border text-center items-center"
-                key={`cell-${rowIdx}-${colIdx}`}
-              >
-                {col}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+      <h1 className="mb-4 text-3xl font-bold">{`Let's play SUDOKU! 📝🤔`}</h1>
+      <h2 className="mb-4 text-2xl font-bold">Select Puzzles</h2>
+      {puzzles.map((puzzle, idx) => (
+        <Link
+          href="/puzzle"
+          className="mb-4 block cursor-pointer rounded-md border p-4 shadow-md hover:shadow-lg"
+          key={puzzle.id}
+        >
+          <span className="font-bold">Puzzle No. {idx + 1}</span>
+        </Link>
+      ))}
     </div>
   );
 };
