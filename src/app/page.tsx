93b368acbe53864ca13170df88d30cf29bcb6790
Septian A Tama <sfirst.tama@gmail.com>
2023-03-05 18:42:15 +0700

    Revert "add pagination in home page aka puzzles list"
    This reverts commit 42c2f0c8eea1d7b474be254dc8d09bcda2abc5fc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
 import classNames from 'classnames';
 import Link from 'next/link';
-
-import {
-  ChevronDoubleLeftIcon,
-  ChevronDoubleRightIcon,
-} from '@heroicons/react/24/outline';
+import { ISudoku } from '~/interfaces';
 
 import { determineDifficulties } from '~/lib/sudoku';
 import { supabase } from '~/lib/supabaseClient';
 
-type THomePageProps = {
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
-
-type TRange = {
-  from: number;
-  to: number;
-};
-
-const LIMIT_PAGE = 5;
-
-async function getPuzzles({ from, to }: TRange) {
-  const { data, error, count } = await supabase
-    .from('sudoku_puzzles')
-    .select('*', { count: 'exact' })
-    .range(from, to);
+async function getPuzzles() {
+  const { data, error } = await supabase.from('sudoku_puzzles').select();
 
   // Recommendation: handle errors
   if (error) {
@@ -32,19 +14,11 @@ async function getPuzzles({ from, to }: TRange) {
     throw new Error('Failed to fetch data');
   }
 
-  return { data, count };
+  return data as ISudoku[];
 }
 
-const Home = async ({ searchParams }: THomePageProps) => {
-  const page = searchParams?.page ? parseInt(searchParams.page as string) : 1;
-  const range = {
-    from: (page - 1) * LIMIT_PAGE,
-    to: page * LIMIT_PAGE - 1,
-  };
-
-  const { data: sudokus, count } = await getPuzzles(range);
-  const totalPage = Math.ceil((count as number) / LIMIT_PAGE);
-
+const Home = async () => {
+  const sudokus = await getPuzzles();
   return (
     <>
       <h2 className="mb-4 text-xl font-bold sm:text-2xl">Select Puzzles</h2>
@@ -76,26 +50,6 @@ const Home = async ({ searchParams }: THomePageProps) => {
             </Link>
           );
         })}
-      </div>
-      <div className="my-8 block flex items-center justify-center">
-        {page > 1 && (
-          <Link href={`/?page=${page - 1}`}>
-            <ChevronDoubleLeftIcon className="h-6 w-6 shrink-0" />{' '}
-          </Link>
-        )}
-        <div
-          className={classNames('w-full text-center', {
-            'ml-6': page === 1,
-            'mr-6': page === totalPage,
-          })}
-        >
-          Page {page} of {totalPage}
-        </div>
-        {page < totalPage && (
-          <Link href={`/?page=${page + 1}`}>
-            <ChevronDoubleRightIcon className=" h-6 w-6 shrink-0" />
-          </Link>
-        )}
       </div>
     </>
   );
