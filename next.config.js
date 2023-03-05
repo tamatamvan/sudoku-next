400c8bd7d75e20299fd3044b802af8e00b2fef88
Septian A Tama <sfirst.tama@gmail.com>
2023-03-05 23:06:44 +0700

    configure cache control

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
